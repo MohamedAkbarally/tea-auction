b80fc32625aa3147dc0fc10518188be2dbe44f5c
Mohamed Akbarally <makbarally@student.unimelb.edu.au>
2020-09-29 08:27:29 +0530

    fux jwt 3

--- app.js
+++ app.js
@@ -88,10 +88,10 @@ require("./config/passport")(passport);
 app.use("/css", express.static(path.join(__dirname, "routes/public/css")));
 
 // server properties endpoint page
-app.use(express.static(path.join(__dirname, "client/build")));
+app.use(express.static(path.join(__dirname, "build")));
 
 app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 app.use(index);
---
+++ build/precache-manifest.b78a044061e53e063ae17425d2d03509.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e0310f9c4c63138307c38dec25c5a8c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d219ffd0007f31e7d9dc",
+    "url": "/static/css/main.fb049570.chunk.css"
+  },
+  {
+    "revision": "3f500d40ab526e016717",
+    "url": "/static/js/2.aca70a23.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/static/js/2.aca70a23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d219ffd0007f31e7d9dc",
+    "url": "/static/js/main.820acb93.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "2554ee5910f8bf4a4089ed003bbdfd40",
+    "url": "/static/media/logo-grey.2554ee59.png"
+  }
+]);
